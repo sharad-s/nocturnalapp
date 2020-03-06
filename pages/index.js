bbab32629c66a5e758a8669db08ed73d0bd310c9
sharad-s <sharadshekar@gmail.com>
2020-03-06 03:38:32 -0600

    init commit

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,15 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 const Home = () => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
+      <title>Nocturnal</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
-
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/new?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
-      </div>
+      <h1 className="title">Nocturnal.</h1>
+      <h3> Coming soon.</h3>
     </main>
 
     <footer>
@@ -60,11 +25,11 @@ const Home = () => (
     <style jsx>{`
       .container {
         min-height: 100vh;
-        padding: 0 0.5rem;
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
+        background-color: black;
       }
 
       main {
@@ -74,15 +39,18 @@ const Home = () => (
         flex-direction: column;
         justify-content: center;
         align-items: center;
+        color: #FDF1E7
       }
 
       footer {
         width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
+        height: 50px;
+        // border-top: 1px solid #eaeaea;
         display: flex;
         justify-content: center;
         align-items: center;
+        background-color: #FDF1E7;
+        // color: white;
       }
 
       footer img {
@@ -198,6 +166,6 @@ const Home = () => (
       }
     `}</style>
   </div>
-)
+);
 
-export default Home
+export default Home;
